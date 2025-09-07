ad7712b12ca55dc1477e32b35f9bf97c79926481
Kshamesh <Kshamesh@gmail.com>
2025-09-07 15:18:35 +0530

    Added UI Coverage Util

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,14 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   test: {
-    globals: true,
     environment: "jsdom",
+    globals: true,
     setupFiles: ["./src/setupTests.ts"],
+    coverage: {
+      provider: "istanbul", // or 'v8'
+      reporter: ["text", "html", "lcov"],
+      include: ["src/**/*.{ts,tsx}"],
+      exclude: ["src/**/*.test.{ts,tsx}", "src/setupTests.ts"],
+    },
   },
 });
